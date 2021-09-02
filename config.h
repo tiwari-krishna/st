f6321d405652722dff3a28643ae18a9f7e430569
tiwari-krishna <89651392+tiwari-krishna@users.noreply.github.com>
2021-09-02 14:13:06 +0545

    Update config.h

--- config.h
+++ config.h
@@ -124,7 +124,7 @@ static const char *colorname[] = {
      /* more colors can be added after 255 to use with DefaultXX */
      "#add8e6", /* 256 -> cursor */
      "#555555", /* 257 -> rev cursor*/
-     "#1f1a0d", /* 258 -> bg */
+     "#1c1a1a", /* 258 -> bg */
      "#ebe2cd", /* 259 -> fg */
      "#47aeed", /* 260 -> selection*/
 };
