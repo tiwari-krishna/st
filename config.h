b9e952bc48f5bbc321fd3f368a9250f12f4df94e
tiwari-krishna <89651392+tiwari-krishna@users.noreply.github.com>
2021-11-24 19:39:29 +0545

    Add files via upload

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
- "FontAwesome:pixelsize=13:antialias=true:autohint=true",
+ "Noto Color Emoji:pixelsize=13:antialias=true:autohint=true",
  "Kantipur:pixelsize=15:antialias=true:autohint=true",
  "Symbola:pixelsize=15:antialias=true:autohint=true",
 };
@@ -117,22 +117,25 @@ float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-    "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-    "#cc241d",
-    "#98971a",
-    "#d79921",
-    "#458588",
-    "#b16286",
-    "#689d6a",
-    "#a89984",
-    "#928374",
-    "#fb4934",
-    "#b8bb26",
-    "#fabd2f",
-    "#83a598",
-    "#d3869b",
-    "#8ec07c",
-    "#ebdbb2",
+        /* 8 normal colors */
+   [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+   [1] = "#cc241d", /* red     */
+   [2] = "#98971a", /* green   */
+   [3] = "#d79921", /* yellow  */
+   [4] = "#458588", /* blue    */
+   [5] = "#b16286", /* magenta */
+   [6] = "#689d6a", /* cyan    */
+   [7] = "#a89984", /* white   */
+ 
+   /* 8 bright colors */
+   [8]  = "#928374", /* black   */
+   [9]  = "#fb4934", /* red     */
+   [10] = "#b8bb26", /* green   */
+   [11] = "#fabd2f", /* yellow  */
+   [12] = "#83a598", /* blue    */
+   [13] = "#d3869b", /* magenta */
+   [14] = "#8ec07c", /* cyan    */
+   [15] = "#ebdbb2", /* white   */
      [255] = 0,
      /* more colors can be added after 255 to use with DefaultXX */
      "#add8e6", /* 256 -> cursor */
@@ -147,9 +150,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 259;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
@@ -164,7 +167,7 @@ static unsigned int defaultrcs = 257;
  * 7: Blinking st cursor
  * 8: Steady st cursor
  */
-static unsigned int cursorshape = 1;
+static unsigned int cursorshape = 5;
 static Rune stcursor = 0x2603; /* snowman (U+2603) */
 
 /*
@@ -198,43 +201,6 @@ static uint forcemousemod = ShiftMask;
 * Command used to query unicode glyphs.
 */
 char *iso14755_cmd = "dmenu -w \"$WINDOWID\" -p codepoint: </dev/null";
-
-/*
- * Xresources preferences to load at startup
- */
-ResourcePref resources[] = {
-       { "font",         STRING,  &font },
-       { "color0",       STRING,  &colorname[0] },
-       { "color1",       STRING,  &colorname[1] },
-       { "color2",       STRING,  &colorname[2] },
-       { "color3",       STRING,  &colorname[3] },
-       { "color4",       STRING,  &colorname[4] },
-       { "color5",       STRING,  &colorname[5] },
-       { "color6",       STRING,  &colorname[6] },
-       { "color7",       STRING,  &colorname[7] },
-       { "color8",       STRING,  &colorname[8] },
-       { "color9",       STRING,  &colorname[9] },
-       { "color10",      STRING,  &colorname[10] },
-       { "color11",      STRING,  &colorname[11] },
-       { "color12",      STRING,  &colorname[12] },
-       { "color13",      STRING,  &colorname[13] },
-       { "color14",      STRING,  &colorname[14] },
-       { "color15",      STRING,  &colorname[15] },
-       { "background",   STRING,  &colorname[256] },
-       { "foreground",   STRING,  &colorname[257] },
-       { "cursorColor",  STRING,  &colorname[258] },
-       { "termname",     STRING,  &termname },
-       { "shell",        STRING,  &shell },
-       { "minlatency",   INTEGER, &minlatency },
-       { "maxlatency",   INTEGER, &maxlatency },
-       { "blinktimeout", INTEGER, &blinktimeout },
-       { "bellvolume",   INTEGER, &bellvolume },
-       { "tabspaces",    INTEGER, &tabspaces },
-       { "borderpx",     INTEGER, &borderpx },
-       { "cwscale",      FLOAT,   &cwscale },
-       { "chscale",      FLOAT,   &chscale },
-};
-
 
 /*
  * Internal mouse shortcuts.
--- x.c
+++ x.c
@@ -14,7 +14,6 @@
 #include <X11/keysym.h>
 #include <X11/Xft/Xft.h>
 #include <X11/XKBlib.h>
-#include <X11/Xresource.h>
 
 char *argv0;
 #include "arg.h"
@@ -56,19 +55,6 @@ enum undercurl_slope_type {
    UNDERCURL_SLOPE_DESCENDING = 2,
    UNDERCURL_SLOPE_BOTTOM_CAP = 3
 };
-
-/* Xresources preferences */
-enum resource_type {
-   STRING = 0,
-   INTEGER = 1,
-   FLOAT = 2
-};
-
-typedef struct {
-   char *name;
-   enum resource_type type;
-   void *dst;
-} ResourcePref;
 
 /* X modifiers */
 #define XK_ANY_MOD    UINT_MAX
@@ -874,7 +860,7 @@ void
 xhints(void)
 {
     XClassHint class = {opt_name ? opt_name : "st",
-                       opt_class ? opt_class : "St"};
+                        opt_class ? opt_class : "st"};
 	XWMHints wm = {.flags = InputHint, .input = 1};
 	XSizeHints *sizeh;
 
@@ -1247,6 +1233,8 @@ xinit(int cols, int rows)
     XWindowAttributes attr;
     XVisualInfo vis;
 
+    if (!(xw.dpy = XOpenDisplay(NULL)))
+            die("can't open display\n");
 	xw.scr = XDefaultScreen(xw.dpy);
     xw.vis = XDefaultVisual(xw.dpy, xw.scr);
 
@@ -2580,59 +2568,6 @@ run(void)
 	}
 }
 
-int
-resource_load(XrmDatabase db, char *name, enum resource_type rtype, void *dst)
-{
-   char **sdst = dst;
-   int *idst = dst;
-   float *fdst = dst;
-
-   char fullname[256];
-   char fullclass[256];
-   char *type;
-   XrmValue ret;
-
-   snprintf(fullname, sizeof(fullname), "%s.%s",
-           opt_name ? opt_name : "st", name);
-   snprintf(fullclass, sizeof(fullclass), "%s.%s",
-           opt_class ? opt_class : "St", name);
-   fullname[sizeof(fullname) - 1] = fullclass[sizeof(fullclass) - 1] = '\0';
-
-   XrmGetResource(db, fullname, fullclass, &type, &ret);
-   if (ret.addr == NULL || strncmp("String", type, 64))
-       return 1;
-
-   switch (rtype) {
-   case STRING:
-       *sdst = ret.addr;
-       break;
-   case INTEGER:
-       *idst = strtoul(ret.addr, NULL, 10);
-       break;
-   case FLOAT:
-       *fdst = strtof(ret.addr, NULL);
-       break;
-   }
-   return 0;
-}
-
-void
-config_init(void)
-{
-   char *resm;
-   XrmDatabase db;
-   ResourcePref *p;
-
-   XrmInitialize();
-   resm = XResourceManagerString(xw.dpy);
-   if (!resm)
-       return;
-
-   db = XrmGetStringDatabase(resm);
-   for (p = resources; p < resources + LEN(resources); p++)
-       resource_load(db, p->name, p->type, p->dst);
-}
-
 void
 usage(void)
 {
@@ -2724,11 +2659,6 @@ run:
 
 	setlocale(LC_CTYPE, "");
 	XSetLocaleModifiers("");
-
-    if(!(xw.dpy = XOpenDisplay(NULL)))
-       die("Can't open display\n");
-
-    config_init();
 	cols = MAX(cols, 1);
 	rows = MAX(rows, 1);
 	tnew(cols, rows);
