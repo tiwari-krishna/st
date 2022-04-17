b75039d0f18e2eee0c99febe2c537b5765847309
Krishna <krishnaptiwari1@gmail.com>
2022-04-17 08:07:11 +0545

    Nothing

--- config.h
+++ config.h
@@ -5,11 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monospace:pixelsize=16:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"Noto Color Emoji:pixelsize=13:antialias=true:autohint=true", 
-	"Hack Nerd Font Mono:pixelsize=14:antialias=true:autohint=true",
+	"Hack Nerd Font:pixelsize=14:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
@@ -112,7 +112,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.9;
+float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
